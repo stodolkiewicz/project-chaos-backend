65f88acc2521f05dc3c8c26de844aaa46efb7d9b
stodo <stodoo@gmail.com>
2025-02-13 22:48:15 +0100

    initial commit. oauth2 with google set up.

---
+++ src/main/java/com/stodo/social/SocialApplication.java
@@ -0,0 +1,15 @@
+package com.stodo.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+
+//	https://spring.io/guides/tutorials/spring-boot-oauth2
+//	https://docs.spring.io/spring-security/reference/servlet/oauth2/login/core.html
+}
