1e39f9405bbfbca25bb8312583772b2f0ce8f2e3
stodo <stodoo@gmail.com>
2025-04-24 00:24:34 +0200

    SpringSecurityAuditorAware bug during login fixed

--- src/main/java/com/stodo/projectchaos/security/config/SpringSecurityAuditorAware.java
+++ src/main/java/com/stodo/projectchaos/security/config/SpringSecurityAuditorAware.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -19,6 +20,11 @@ public class SpringSecurityAuditorAware implements AuditorAware<String> {
         if (authentication == null || !authentication.isAuthenticated() ||
                 authentication instanceof AnonymousAuthenticationToken) {
             return Optional.empty();
+        }
+
+        // fires up during Google oauth2 login also, but we don't need the user then
+        if (authentication.getPrincipal() instanceof DefaultOidcUser) {
+            return null;
         }
 
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
