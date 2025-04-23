843d0df9017161c82601aeac98b1ea958c305e5c
stodo <stodoo@gmail.com>
2025-04-23 22:59:29 +0200

    TaskComments

---
+++ src/main/java/com/stodo/projectchaos/model/entity/TaskComments.java
@@ -0,0 +1,30 @@
+package com.stodo.projectchaos.model.entity;
+
+import com.stodo.projectchaos.model.entity.superclass.Auditable;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "task_comments")
+public class TaskComments extends Auditable {
+    @Id
+    private UUID id = UUID.randomUUID();
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "task_id", nullable = false)
+    private TaskEntity task;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "author_id", nullable = false)
+    private UserEntity author;
+
+    @Column(nullable = false)
+    private String content;
+}
