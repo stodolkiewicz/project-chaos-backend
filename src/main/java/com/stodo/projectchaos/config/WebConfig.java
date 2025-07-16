c0ecd2a6c0b9d1091db96e1e0bd1b9a4fe8d9940
stodo <stodoo@gmail.com>
2025-07-17 01:30:47 +0200

    debug

---
+++ src/main/java/com/stodo/projectchaos/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.stodo.projectchaos.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Profile("prod")
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>(new ForwardedHeaderFilter());
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
